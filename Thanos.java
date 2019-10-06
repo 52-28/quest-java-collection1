e5036ce7a2c2e567d9dff7a61bdf4c6bfde98f11
52-28 <aymeric.feillault@gmail.com>
2019-10-06 12:06:24 +0200

    Resolution

--- Thanos.java
+++ Thanos.java
@@ -4,25 +4,42 @@ public class Thanos {
 
     public static void main(String[] args) {
 
-        // TODO 1 : Create an empty heroes list
+	//Création de la liste vide
+	ArrayList<Hero> heroes = new ArrayList<>();
 
-        // TODO 2 : Add those heroes to the list
-        // name: Black Widow, age: 34
-        // name: Captain America, age: 100
-        // name: Vision, age: 3
-        // name: Iron Man, age: 48
-        // name: Scarlet Witch, age: 29
-        // name: Thor, age: 1500
-        // name: Spider-Man, age: 18
-        // name: Hulk, age: 49
-        // name: Doctor Strange, age: 42
+	//création des heros
+	Hero blackWidow = new Hero("Black Widow",34);
+	Hero captainAmerica = new Hero("Captain America",100);
+	Hero vision = new Hero("Vision",3);
+	Hero ironMan = new Hero("Iron Man",48);
+	Hero scarletWitch = new Hero("Scarlet Witch",29);
+	Hero thor = new Hero("Thor",1500);
+	Hero spiderMan = new Hero("Spider-Man",18);
+	Hero hulk = new Hero("Hulk",49);
+	//Hero doctorStrange = new Hero("Doctor Strange",42);
 
-        // TODO 3 : It's Thor birthday, now he's 1501
+	//ajout des heros a la liste
+	heroes.add(blackWidow);
+	heroes.add(captainAmerica);
+	heroes.add(vision);
+	heroes.add(ironMan);
+	heroes.add(scarletWitch);
+	heroes.add(thor);
+	heroes.add(spiderMan);
+	heroes.add(hulk);
+	//heroes.add(doctorStrange);
 
-        // TODO 4 : Shuffle the heroes list
+	//changement de l'age de Thor
+	thor.setAge(thor.getAge()+1);
 
-        // TODO 5 : Keep only the half of the list
+	//On mélange la list
+	Collections.shuffle(heroes);
+        
+	// TODO 5 : Keep only the half of the list
+
+	List<Hero> newHeroesList = heroes.subList(0,heroes.size()/2);
 
         // TODO 6 : Loop throught the list and display the name of the remaining heroes
+	newHeroesList.forEach((hero) -> System.out.println(hero.getName()));
     }
 }
